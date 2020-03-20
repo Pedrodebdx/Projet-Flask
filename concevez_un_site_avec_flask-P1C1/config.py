e440ae21b2509feea2ceec2f468e859b3be40244
Pedrodebdx <pv.ferrat@gmail.com>
2020-03-20 16:47:22 +0100

    ajout app fb

--- concevez_un_site_avec_flask-P1C1/config.py
+++ concevez_un_site_avec_flask-P1C1/config.py
@@ -1,3 +1,10 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'C:\sqlite\bdd_sqlite3_appfb.db')
+
+
+
 FB_APP_ID = 2396776397091777
 
 SECRET_KEY = "#d#JCqTTW\nilK\\7m\x0bp#\tj~#H"
--- concevez_un_site_avec_flask-P1C1/fbapp/views.py
+++ concevez_un_site_avec_flask-P1C1/fbapp/views.py
@@ -2,11 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Config options - Make sure you created a 'config.py' file.
 app.config.from_object('config')
+# To get one variable, tape app.config['MY_VARIABLE']
 
 @app.route('/')
 def index():
-    return "Hello world !"
+        return "Hello world !"
 
 if __name__ == "__main__":
-    app.run()
+        app.run()
