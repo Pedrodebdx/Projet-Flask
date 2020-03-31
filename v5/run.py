2cbde8d11ccbf37e142cb9b80b50295da554156b
Pedrodebdx <pv.ferrat@gmail.com>
2020-03-31 20:46:24 +0200

    db ok mais pas verif si id deja dans bdd

--- v5/run.py
+++ v5/run.py
@@ -1,17 +1,10 @@
 from flask import Flask, render_template, request
+import pymongo
+
 
 app = Flask(__name__)
-app.config.from_object(__name__) # load config from this file , flaskr.py
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
-))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-   
-   
+
 @app.route('/')
 def home():
     return render_template("home.html")
@@ -20,37 +13,27 @@ def home():
 def text_box():
     nom = request.form['nom'].upper()
     prenom = request.form['prenom']
-    age = request.form['age']
-    sexe = request.form['sexe']
+    pseudo = request.form['pseudo']
+    #données récoltées 
+    data = {'nom': nom, 'prenom':prenom, '_id': pseudo, 'pseudo': pseudo}
+
+    import pymongo
+    # connection à mongo 
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    #création de la base de données imdb
+    mydb = myclient["flaskv5"]
+    #création de la collection 'titles'
+    mycol = mydb["users"]
+    #insertion des données
     
-    return render_template("bienvenue.html", 
-    prenom=prenom, nom=nom, age=age, sexe=sexe)
-    
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+    if data['pseudo'] == mycol.find({ "_id": pseudo }) :
+        return render_template("erreur.html")
+    else:
+        x = mycol.insert_one(data)
 
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
+    return render_template("bienvenue.html", data= data)
 
-@app.cli.command('initdb')
-def initdb_command():
-    """Initializes the database."""
-    init_db()
-    print('Initialized the database.')
 
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
 
 if __name__ == '__main__':
     app.run()
